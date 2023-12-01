875b80d8d100bfc6df7fa50c41744890df7deb91
Sean DeBarr <sdebarr@innerspec.com>
2023-12-01 11:24:34 -0500

    2023 day-01 part 1

---
+++ 2023/day-01/src/bin/part1.rs
@@ -0,0 +1,43 @@
+fn main() {
+    let input: &str = include_str!("../input.txt");
+    println!("{}", solution(input));
+}
+
+fn solution(input: &str) -> i32 {
+    let mut total: i32 = 0;
+
+    for line in input.lines() {
+        let test = line
+            .chars()
+            .map(|c| c.to_digit(10).unwrap_or(10))
+            .filter(|n| *n != 10)
+            .collect::<Vec<u32>>();
+
+        println!("{:?} ", test);
+
+        let first = test.first().unwrap().to_string();
+        let last = test.last().unwrap().to_string();
+
+        let number = format!("{}{}", first, last).parse::<i32>().unwrap();
+
+        total += number;
+    }
+
+    return total;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = solution(
+            "1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet",
+        );
+        assert_eq!(result, 142);
+    }
+}
